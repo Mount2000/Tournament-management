db6549a21886803fe4c7be8a6df2c7976b019c12
Mai Hong Son <maihongson151120@gmail.com>
2024-08-01 16:52:06 +0700

    delete useless variable in TournamentManagement contract

--- contracts/TournamentFactory.sol
+++ contracts/TournamentFactory.sol
@@ -3,7 +3,10 @@ contract TournamentFactory{
     string public name;
     uint public timeStart;
     uint public timeEnd;
+    uint public fee;
+    uint public award;
     mapping (address => bool) managers;
+    mapping (address => bool) players;
 
     constructor(){
         managers[msg.sender] = true;
@@ -13,15 +16,21 @@ contract TournamentFactory{
         managers[manager] = isManager;
     }
 
-    function setTournament(string memory _name, uint _timeStart, uint _timeEnd) external onlyManager{
+    function setTournament(string memory _name, uint _timeStart, uint _timeEnd, uint _fee, uint _award) external onlyManager{
         require(_timeStart > block.timestamp, "Tournament has to start at the future");
         require(_timeEnd > _timeStart, "Tournament has to end after start");
         name = _name;
         timeStart = _timeStart;
         timeEnd = _timeEnd;
+        fee = _fee;
+        award = _award;
     }
+
     function getTournament() public view returns(string memory, uint, uint){
         return (name, timeStart, timeEnd);
+    }
+    function getFee() public view returns(uint){
+        return fee;
     }
     modifier onlyManager{
         require(managers[msg.sender], "Only manager can impliment this fuction");
--- contracts/TournamentManagement.sol
+++ contracts/TournamentManagement.sol
@@ -5,28 +5,41 @@ import "hardhat/console.sol";
 import "./TournamentFactory.sol";
 
 contract TournamentManagement is Ownable(msg.sender){
-    mapping(address => TournamentFactory) tournamentFactories;
-    event creatTournament(address tournament, string name,uint timeStart,uint timeEnd);
-    event updateTournament(address tournament, string name, uint timeStart, uint timeEnd);
+    struct User{
+        uint id;
+        mapping(address => bool) isPlayer;
+        mapping(address => uint) position;
+    }
+    mapping(address => User) users;
+    event creatTournament(address tournament, string name,uint timeStart,uint timeEnd, uint fee, uint award);
+    event updateTournament(address tournament, string name, uint timeStart, uint timeEnd, uint fee, uint award);
 
-    function creatNewTournament(string memory name, uint timeStart, uint timeEnd) public onlyOwner{
-        TournamentFactory tournament = new TournamentFactory();
-        tournament.setTournament(name, timeStart, timeEnd);
-        tournamentFactories[address(tournament)] = tournament;
-        emit creatTournament(address(tournament), name, timeStart, timeEnd);
-        console.log(address(tournament));
+    function creatNewTournament(string memory name, uint timeStart, uint timeEnd, uint fee, uint award) public onlyOwner{
+        TournamentFactory newTournament = new TournamentFactory();
+        tournament.setTournament(name, timeStart, timeEnd, fee, award);
+        emit creatTournament(address(newTournament), name, timeStart, timeEnd, fee, award);
     }
 
-    function editTournament(address tournament, string memory name, uint timeStart, uint timeEnd) public onlyOwner{
-        tournamentFactories[tournament].setTournament(name, timeStart, timeEnd);
-        emit updateTournament(tournament, name, timeStart, timeEnd);
+    function editTournament(address tournament, string memory name, uint timeStart, uint timeEnd, uint fee, uint award) public onlyOwner{
+        TournamentFactory(tournament).setTournament(name, timeStart, timeEnd, fee, award);
+        emit updateTournament(tournament, name, timeStart, timeEnd, fee, award);
     }
 
     function setManagerTournament(address tournament, address manager, bool isManager) public onlyOwner{
-        tournamentFactories[tournament].setManager(manager, isManager);
+        TournamentFactory(tournament).setManager(manager, isManager);
     }
 
     function getTournament(address tournament) public view returns(string memory, uint, uint){
-        return(tournamentFactories[tournament].getTournament());
+        return(TournamentFactory(tournament).getTournament());
+    }
+
+    function joinTournament(address tournament) payable public{
+        uint fee = TournamentFactory(tournament).getFee();
+        uint overFee = msg.value - fee;
+        require(msg.value >= fee, "do not enough fee to join");
+        users[msg.sender].isPlayer[tournament] = true;
+        if(overFee != 0){
+            payable(msg.sender).transfer(overFee);
+        }
     }
 }
