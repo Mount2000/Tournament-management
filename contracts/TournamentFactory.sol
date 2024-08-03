1cffbc97ffbcb3c78700c5ee2046efc39b262b29
Mai Hong Son <maihongson151120@gmail.com>
2024-08-03 23:54:50 +0700

    full function and test with remix

--- contracts/TournamentFactory.sol
+++ contracts/TournamentFactory.sol
@@ -1,39 +1,72 @@
 pragma solidity ^0.8.24;
-contract TournamentFactory{
-    string public name;
-    uint public timeStart;
-    uint public timeEnd;
-    uint public fee;
-    uint public award;
-    mapping (address => bool) managers;
-    mapping (address => bool) players;
 
-    constructor(){
-        managers[msg.sender] = true;
+import {AccessControl} from "node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+
+contract TournamentFactory is AccessControl{
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    bytes32 public constant REFEREE_ROLE = keccak256("REFEREE_ROLE");
+    bytes32 public constant PLAYER_ROLE = keccak256("PLAYER_ROLE");
+
+    struct Tournament{
+    string name;
+    uint timeStart;
+    uint timeEnd;
+    uint fee;
+    uint award;
+    }
+    Tournament public tournament;
+
+    mapping(address => uint) public position;
+    uint64 countPlayer;
+    address public winner;
+
+    event updateTournament(string name, uint timeStart, uint timeEnd, uint fee, uint award);
+    event _winner(address player);
+    constructor(string memory _name, uint _timeStart, uint _timeEnd, uint _fee, uint _award){
+        _grantRole(MANAGER_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        setTournament(_name, _timeStart, _timeEnd, _fee, _award);
     }
 
-    function setManager(address manager, bool isManager) external onlyManager{
-        managers[manager] = isManager;
-    }
-
-    function setTournament(string memory _name, uint _timeStart, uint _timeEnd, uint _fee, uint _award) external onlyManager{
+    function setTournament(string memory _name, uint _timeStart, uint _timeEnd, uint _fee, uint _award) public onlyRole(DEFAULT_ADMIN_ROLE) onlyBeforTournamentStart{
         require(_timeStart > block.timestamp, "Tournament has to start at the future");
         require(_timeEnd > _timeStart, "Tournament has to end after start");
-        name = _name;
-        timeStart = _timeStart;
-        timeEnd = _timeEnd;
-        fee = _fee;
-        award = _award;
+        tournament.name = _name;
+        tournament.timeStart = _timeStart;
+        tournament.timeEnd = _timeEnd;
+        tournament.fee = _fee;
+        tournament.award = _award;
+
+        emit updateTournament(_name, _timeStart, _timeEnd, _fee, _award);
+    }
+    
+    function setManager(address manager) external onlyRole(DEFAULT_ADMIN_ROLE){
+        _grantRole(MANAGER_ROLE, manager);
     }
 
-    function getTournament() public view returns(string memory, uint, uint){
-        return (name, timeStart, timeEnd);
+    function setReferee(address referee) external onlyRole(DEFAULT_ADMIN_ROLE){
+        _grantRole(REFEREE_ROLE, referee);
     }
-    function getFee() public view returns(uint){
-        return fee;
+
+    function setPlayer(address player, uint _position) external onlyRole(DEFAULT_ADMIN_ROLE) onlyBeforTournamentStart{
+        require(!hasRole(PLAYER_ROLE, player),"This address is already player");
+        countPlayer ++;
+        _grantRole(PLAYER_ROLE, player);
+        position[player] = countPlayer * 1000 + _position;
     }
-    modifier onlyManager{
-        require(managers[msg.sender], "Only manager can impliment this fuction");
+
+    function setWinner(address player) external onlyRole(DEFAULT_ADMIN_ROLE) onlyAfterTournamentEnd{
+        winner = player;
+        emit _winner(player);
+    }
+
+    modifier onlyBeforTournamentStart{
+        require(tournament.timeStart > block.timestamp, "Tournament has aldready started");
+        _;
+    }
+    modifier onlyAfterTournamentEnd{
+        require(tournament.timeEnd < block.timestamp, "Tournament did not end");
         _;
     }
 }
--- contracts/TournamentManagement.sol
+++ contracts/TournamentManagement.sol
@@ -1,45 +1,117 @@
 pragma solidity ^0.8.24;
 
 import {Ownable} from "node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "node_modules/@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 import "hardhat/console.sol";
 import "./TournamentFactory.sol";
 
-contract TournamentManagement is Ownable(msg.sender){
-    struct User{
-        uint id;
-        mapping(address => bool) isPlayer;
-        mapping(address => uint) position;
+contract TournamentManagement is VRFConsumerBaseV2Plus{
+
+    bytes32 s_keyHash;
+    address Coordinator;
+    uint s_subscriptionId;
+    uint s_randomWords;
+    uint s_requestId;
+
+    address payable withdrawWallet;
+    uint totalAwards;
+
+
+    event creatTournament(address tournament);
+    event ReturnedRandomness(uint256 randomWords);
+    event _withdrawAward(address tournament);
+
+    constructor( bytes32 _keyHash, address _Coordinator, uint _SubscriptionID ) VRFConsumerBaseV2Plus(Coordinator){
+        s_keyHash = _keyHash;
+        Coordinator = _Coordinator;
+        s_subscriptionId = _SubscriptionID;
+        withdrawWallet = payable(msg.sender);
     }
-    mapping(address => User) users;
-    event creatTournament(address tournament, string name,uint timeStart,uint timeEnd, uint fee, uint award);
-    event updateTournament(address tournament, string name, uint timeStart, uint timeEnd, uint fee, uint award);
 
     function creatNewTournament(string memory name, uint timeStart, uint timeEnd, uint fee, uint award) public onlyOwner{
-        TournamentFactory newTournament = new TournamentFactory();
-        tournament.setTournament(name, timeStart, timeEnd, fee, award);
-        emit creatTournament(address(newTournament), name, timeStart, timeEnd, fee, award);
+        totalAwards += award;
+        require(address(this).balance >= totalAwards, "Not enough token to creat award");
+        TournamentFactory newTournament = new TournamentFactory(name, timeStart, timeEnd, fee, award);
+        emit creatTournament(address(newTournament));
     }
 
     function editTournament(address tournament, string memory name, uint timeStart, uint timeEnd, uint fee, uint award) public onlyOwner{
         TournamentFactory(tournament).setTournament(name, timeStart, timeEnd, fee, award);
-        emit updateTournament(tournament, name, timeStart, timeEnd, fee, award);
     }
 
-    function setManagerTournament(address tournament, address manager, bool isManager) public onlyOwner{
-        TournamentFactory(tournament).setManager(manager, isManager);
+    function setManagerTournament(address tournament, address manager) public onlyOwner{
+        TournamentFactory(tournament).setManager(manager);
     }
 
-    function getTournament(address tournament) public view returns(string memory, uint, uint){
-        return(TournamentFactory(tournament).getTournament());
+    function setRefereeTournament(address tournament, address referee) public onlyTournamentManager(tournament){
+        TournamentFactory(tournament).setReferee(referee);
     }
 
-    function joinTournament(address tournament) payable public{
-        uint fee = TournamentFactory(tournament).getFee();
-        uint overFee = msg.value - fee;
-        require(msg.value >= fee, "do not enough fee to join");
-        users[msg.sender].isPlayer[tournament] = true;
-        if(overFee != 0){
-            payable(msg.sender).transfer(overFee);
-        }
+    function joinTournament(address tournament, address player) public payable onlyOwner{
+        uint fee;
+        (,,,fee,)= TournamentFactory(tournament).tournament();
+        require(msg.value == fee, "transfer value did not match with fee");
+        requestRandomWords();
+        TournamentFactory(tournament).setPlayer(player, s_randomWords);
+    }
+
+    function setWinnerTournament(address tournament, address referee) public {
+        require(TournamentFactory(tournament).hasRole(keccak256("REFEREE_ROLE"), msg.sender), "Only referee can do this function");
+        TournamentFactory(tournament).setReferee(referee);
+    }    
+
+    function withdrawAward(address tournament, address winner) public onlyOwner{
+        require(TournamentFactory(tournament).winner() != address(0), "This tournament did not have winner");
+        require(TournamentFactory(tournament).winner() == winner, "This address are not winner of this tournament");
+        uint award;
+        (,,,,award) = TournamentFactory(tournament).tournament();
+        totalAwards -= award;
+        payable(winner).transfer(award);
+        emit _withdrawAward(tournament);
+    }
+
+    function setWithdrawWallet(address payable _withdrawWallet) public onlyOwner{
+        withdrawWallet = _withdrawWallet;
+    }
+
+    function withdrawTournamentFee(uint amout) public onlyOwner{
+        require(address(this).balance >= amout + totalAwards,"Contract balance has to be more than total award");
+        withdrawWallet.transfer(amout);
+    }
+
+    // get an array random words
+    function requestRandomWords() internal {
+        // Will revert if subscription is not set and funded.
+        s_requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: 3,
+                callbackGasLimit: 100000,
+                numWords: 1,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    // get value and assign first element of random array to s_randomWords
+    function fulfillRandomWords(
+        uint256 /* requestId */,
+        uint256[] calldata randomWords
+    ) internal override {
+        s_randomWords = randomWords[0]%1000;
+        emit ReturnedRandomness(s_randomWords);
+    }
+
+    receive() external payable { }
+    fallback() external payable {}
+
+    modifier onlyTournamentManager(address tournament){
+        require(TournamentFactory(tournament).hasRole(keccak256("MANAGER_ROLE"), msg.sender), "Only manager can do this function");
+        _;
     }
 }
